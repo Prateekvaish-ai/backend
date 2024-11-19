f28b856d0f8bb1a02013704aab96ce12decef04e
Prateekvaish-ai <prateekvaish449@gmail.com>
2024-11-19 15:59:29 +0530

    delete, update models created in backend

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -26,11 +26,50 @@ router.get('/getall', (req, res) => {
         console.log(err);
         res.status(500).json(err);
     });
-})
+});
+
+// : denotes url parameter
+router.get('/getbycity/:city', (req, res) => {
+    Model.find({ city: req.params.city })
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // getbyid
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id)
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // delete
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 // update
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, {new: true })
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
