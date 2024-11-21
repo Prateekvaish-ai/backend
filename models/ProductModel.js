cd1dac6a597eab1176c9ca489e4065a02c5aa4a3
Prateekvaish-ai <prateekvaish449@gmail.com>
2024-11-21 20:38:23 +0530

    create product model

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -2,12 +2,11 @@ const { Schema, model } = require('../connection');
 
 const mySchema = new Schema({
     name: String,
-    email: { type : String, unique: true },
-    password: { type : String, required: true },
-    city: {type : String, default:'Unknown'},
+    price: Number,
+    brand: { type: String },
+    image: { type: String },
+    category: { type: String, default: 'unspecified' },
     createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = model('users', mySchema );
-
- 
+module.exports = model('products', mySchema);
